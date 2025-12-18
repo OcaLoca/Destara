ec0a5eddd269ea2471e09fe6df16b7d8d27d3bb9
Nicolò Comi <nicocomi1992@gmail.com>
2025-12-18 16:33:55 +0100

    [Project] Added keyboards shoutcourt

--- Assets/Scripts/AMVCC/GameController.cs
+++ Assets/Scripts/AMVCC/GameController.cs
@@ -1,12 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using Game;
 using SmartMVC;
+using UnityEngine;
+using UnityEngine.UI;
 
-namespace Game
+public class GameController : Controller<GameApplication>
 {
-    public class GameController : Controller<GameApplication>
-    {
+    public bool activeKeyboards = true;
 
+    [SerializeField] private Scrollbar verticalScrollbar;
+
+    [Header("Scroll Settings")]
+    [SerializeField] private float scrollSpeed = 0.6f; // velocità
+    [SerializeField] private float smoothTime = 0.08f; // fluidità
+
+    private float scrollVelocity;
+
+    private void Update()
+    {
+        if (!activeKeyboards || verticalScrollbar == null)
+            return;
+
+        float input = 0f;
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            input = -1f;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            input = 1f;
+
+        if (input != 0f)
+            SmoothScroll(input);
+    }
+
+    private void SmoothScroll(float direction)
+    {
+        float target = verticalScrollbar.value + direction * scrollSpeed * Time.deltaTime;
+
+        verticalScrollbar.value = Mathf.SmoothDamp(
+            verticalScrollbar.value,
+            Mathf.Clamp01(target),
+            ref scrollVelocity,
+            smoothTime
+        );
     }
 }
